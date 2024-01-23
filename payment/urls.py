e35be38210e88251b5a77674956b0b9c111f0655
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2024-01-23 11:40:31 +0530

    make it a new url for pyment

--- payment/urls.py
+++ payment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create-checkout-session/<int:courseId>/<str:user>/', StripeCheckoutView.as_view())
+    path('create-checkout-session/<int:courseId>/<str:user>/', StripeCheckoutView.as_view()),
+    path('success-checkout/',SuccessCheckOut.as_view(),name='success_checkout'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -8,6 +8,7 @@ from course.models import Course
 from django.utils import timezone
 from django.shortcuts import redirect
 import stripe
+from rest_framework import status
 
 base_url = settings.BASE_URL
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -20,6 +21,7 @@ class StripeCheckoutView(APIView):
             user_instance = User.objects.get(username=user)
             course = Course.objects.get(id=courseId)  
             coursePrice = course.price
+            course_instructor = course.instructor
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
@@ -36,23 +38,53 @@ class StripeCheckoutView(APIView):
                 ],
                 payment_method_types=['card',],
                 mode='payment',
-                success_url=settings.SITE_URL + '/mylearnings/?success=true&session_id={CHECKOUT_SESSION_ID}',
+                # success_url=settings.SITE_URL + '/mylearnings/?success=true&session_id={CHECKOUT_SESSION_ID}',
+                success_url=f'https://akbarali.shop/stripe/success-checkout/?success=true&session_id={{CHECKOUT_SESSION_ID}}&course={course}&coursePrice={coursePrice}$user_instance={user_instance}&course_instructor={course_instructor}"',
                 cancel_url=settings.SITE_URL + '/?canceled=true',
             )
             
-            payment = Payment.objects.create(
-                course=course,
-                user=user_instance,
-                chef=course.instructor,
-                amount=coursePrice,
-                status='success',
-                payed=True,
-                payment_date=timezone.now(),
-            )
-            payment.save()
+            # payment = Payment.objects.create(
+            #     course=course,
+            #     user=user_instance,
+            #     chef=course.instructor,
+            #     amount=coursePrice,
+            #     status='success',
+            #     payed=True,
+            #     payment_date=timezone.now(),
+            # )
+            # payment.save()
             
             return redirect(checkout_session.url)
         except Course.DoesNotExist:
             return Response({'error': 'Course not found'})
         except Exception as e:
             return Response({'error': str(e)}, status=500)
+
+class SuccessCheckOut(APIView):
+    def get(self, request):
+        try:
+            course = request.query_params.get('course')  
+            coursePrice = request.query_params.get('coursePrice')
+            user_instance = request.query_params.get('user_instance')
+            course_instructor = request.query_params.get('course_instructor')
+
+            print(f'course: {course}')
+            print(f'user_price: {coursePrice}')
+            print(f'user_instance: {user_instance}')
+            print(f'course_instructor: {course_instructor}')
+
+            payment = Payment.objects.create(
+                course=course,
+                user=user_instance,
+                chef=course_instructor,
+                amount=coursePrice,
+                status='success',
+                payed=True,
+                payment_date=timezone.now(),
+            )
+            payment.save()
+            success_url=settings.SITE_URL + '/mylearnings/?success=true&session_id={CHECKOUT_SESSION_ID}',
+            return redirect(success_url)
+        except Exception as e:
+            print(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
